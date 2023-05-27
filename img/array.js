9254cff868c4d13dabb6cb6846ed56beff403cec
davidnzoli <davidzolin93@gmail.com>
2023-05-27 14:50:13 +0200

    First commit

---
+++ img/array.js
@@ -0,0 +1,8 @@
+
+  const button = document.querySelector('#button');
+           const div = document.querySelector('.div');
+            button.addEventListener('keydown', (ok)=>{
+           document.body.style.backgroundColor='red';
+            div.textContent='man tu es où';
+        });
+  
